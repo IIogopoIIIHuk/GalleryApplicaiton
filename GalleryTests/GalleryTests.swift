7be57c6ab8cf598dda75c3a88c386f4a2be94120
aliaksandrMamchyts <sasha.mamchits19@gmail.com>
2025-08-23 18:30:29 +0300

    Initial Commit

---
+++ GalleryTests/GalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryTests.swift
+//  GalleryTests
+//
+//  Created by Александр Мамчиц on 8/23/25.
+//
+
+import Testing
+@testable import Gallery
+
+struct GalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
